40d70a04835b43d19c669d85f1d306be714c0be3
Christian Smith <cgr.gonzalezrossier@gmail.com>
2020-12-10 23:01:44 -0300

    Se hicieron cambios menores

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,17 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss'],
 })
-export class SectionComponent implements OnInit {
-  @Input() 
-  titleContent = "sin titulo"; 
-  description="sin descripcion";
+export class SectionComponent {
+  @Input() public titleContent = 'Sin título';
+  @Input() public description = 'Sin descripción';
+  @Input() public imgUrl: string = null;
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor() {}
 }
--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { User } from '@services/login.service';
 
+interface SettingsOption {
+  titleContent: string;
+  imgUrl: string;
+  description: string;
+}
 @Component({
   selector: 'app-tab2',
   templateUrl: 'settings.page.html',
   styleUrls: ['settings.page.scss'],
 })
 export class SettingsPage {
+  public options: SettingsOption[] = [
+    {
+      titleContent: 'Perfil',
+      imgUrl: 'assets/ion-icons/user.png',
+      description: 'Nombre, Apellido, Rut, Tipo Usuario, Cambiar Clave...',
+    },
+    {
+      titleContent: 'Administrar',
+      imgUrl: 'assets/ion-icons/setting.png',
+      description: 'Agregar Usuarios, Modificar Usuarios, Ver Órdenes...',
+    },
+    {
+      titleContent: 'Ir a TallerPrincipal.cl',
+      imgUrl: 'assets/ion-icons/web.png',
+      description: 'Redirige a la pagina web',
+    },
+    {
+      titleContent: 'Ayuda',
+      imgUrl: 'assets/ion-icons/help.png',
+      description: 'Preguntas frecuentes y otras ayudas',
+    },
+  ];
+  public user: User = JSON.parse(atob(localStorage.getItem('idrt'))).user;
+
   constructor() {}
+
+  public doSomething(index: number) {
+    switch (index) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        open('http://tallerprincipal.cl/', '_blank');
+        break;
+      default:
+        break;
+    }
+    console.log(index);
+  }
 }
